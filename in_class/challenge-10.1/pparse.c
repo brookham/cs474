ca305a2f7dee309ffafa226d6a68262198a51c68
brookham <bhamilton3264@gmail.com>
2026-06-02 17:57:58 -0700

    10.1

---
+++ in_class/challenge-10.1/pparse.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+/*
+ * Accepts an absolute path. Prints `/` followed by each path element in
+ * turn.
+ */
+void loop_path(char *path)
+{
+    char *token;
+
+    token = strtok(path, "/");
+
+    while (token){
+        printf("%s\n", token);
+
+        token = strtok(NULL, "/");
+    }
+
+    
+}
+
+#define TEST_PATH_COUNT 4
+
+int main(void)
+{
+    char paths[TEST_PATH_COUNT][32] = {
+        "/",
+        "/foo",
+        "/foo/bar",
+        "/foo/bar/../baz"
+    };
+
+    for (int i = 0; i < TEST_PATH_COUNT; i++) {
+        printf("%d: %s:\n", i, paths[i]);
+        loop_path(paths[i]);
+        putchar('\n');
+    }
+}
